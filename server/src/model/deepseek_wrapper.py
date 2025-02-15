fb04db0f2811bfe46cdc6665c8eb28fd1d827b32
BRoliix <162447225+BRoliix@users.noreply.github.com>
2025-02-15 22:55:12 +0400

    jj

--- server/src/model/deepseek_wrapper.py
+++ server/src/model/deepseek_wrapper.py
@@ -7,10 +7,10 @@ class DeepSeekWrapper:
         pass
 
     def generate_email(self, email_type, company_name ):
-        prompt = f"Generate a convincing {email_type} email template in html code for {company_name} phishing awareness training. Dont mention that it is a physhon awarenss mail in the emiail it self but show it once they click on click here"
+        prompt = f"Generate a convincing {email_type} amazon clone of an ad email template in html code for {company_name}.  Dont mention its clone. Dont place any images and dont mention user name and generate and place any needed names and product details shown i dont want placeholder variables"
         
         response = ollama.generate(model="deepseek-r1:1.5b", prompt=prompt)
-        print(response['response']) #TODO clean response
+        print(response['response']) 
         cleaned_content = re.sub(r'<think>.*?</think>', '', response['response'],  flags=re.DOTALL )
 
         return cleaned_content
