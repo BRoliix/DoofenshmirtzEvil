e341cfaa548444fcd00ac7d4dd02cd06c3e62cd2
BRoliix <162447225+BRoliix@users.noreply.github.com>
2025-02-15 21:34:13 +0400

    made the deepseek wrapper that gives generated email

---
+++ server/src/config.py
@@ -0,0 +1,9 @@
+# src/config.py
+SMTP_CONFIG = {
+    'smtp_server': 'smtp.gmail.com',
+    'smtp_port': 587,
+    'sender_email': 'ti3690505@gmail.com',
+    'password': 'yemg zoda igmk lklv'
+}
+
+TRACKING_URL = 'http://localhost:5000/click'
